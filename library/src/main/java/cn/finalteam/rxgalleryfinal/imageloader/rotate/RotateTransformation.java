aca324193b2c5d0bc698255fbca08fec5128389d
shiyq <shiyq@51lick.cn>
2020-04-02 14:14:05 +0800

    Initial commit

---
+++ library/src/main/java/cn/finalteam/rxgalleryfinal/imageloader/rotate/RotateTransformation.java
@@ -0,0 +1,40 @@
+package cn.finalteam.rxgalleryfinal.imageloader.rotate;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation;
+
+import java.security.MessageDigest;
+
+/**
+ * Created by pengjianbo  Dujinyang on 2016/8/16 0016.
+ */
+public class RotateTransformation extends BitmapTransformation {
+
+    private float rotateRotationAngle = 0f;
+
+    public RotateTransformation(Context context, float rotateRotationAngle) {
+        super();
+        this.rotateRotationAngle = rotateRotationAngle;
+    }
+
+    @Override
+    protected Bitmap transform(BitmapPool pool, Bitmap toTransform, int outWidth, int outHeight) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(rotateRotationAngle);
+        return Bitmap.createBitmap(toTransform, 0, 0, toTransform.getWidth(), toTransform.getHeight(), matrix, true);
+    }
+
+    public String getId() {
+        return "rotate" + rotateRotationAngle;
+    }
+
+    @Override
+    public void updateDiskCacheKey(@NonNull MessageDigest messageDigest) {
+
+    }
+}
